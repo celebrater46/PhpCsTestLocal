ddf95ae6f29762f04614554c1c6ded132cae3fb3
hideru <jota.16.stigma.world@gmail.com>
2021-06-15 20:34:57 +0900

    First Commit

---
+++ PhpCsTest/Form1.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhpCsTest
+{
+    public partial class Form1 : Form
+    {
+        private Label lb;
+        private Button btPut;
+        private Button btGet;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            this.Text = "サンプル";
+            this.Width = 250;
+            this.Height = 100;
+
+            lb = new Label();
+            lb.Text = "C#よりPHPを召喚ッ！！";
+            lb.Width = 150;
+            
+            btPut = new Button();
+            btPut.Text = "データの挿入";
+            btPut.Top = this.Top + lb.Height;
+            btPut.Width = lb.Width;
+
+            btGet = new Button();
+            btGet.Text = "データの呼び出し";
+            btGet.Top = this.Top + lb.Height;
+            btGet.Width = lb.Width;
+
+            btPut.Parent = this;
+            btGet.Parent = this;
+            lb.Parent = this;
+
+            // bt.Click += new EventHandler(bt_Click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Handler.URL = "http://localhost:8080/testPhpCs/index.php";
+        }
+
+        private static void InsertData(string data)
+        {
+            var values = new NameValueCollection();
+            values["type"] = "t_insert_data";
+            values["data"] = data;
+            string result = Handler.DoPost(values);
+            if (result != "")
+            {
+                var jobj = JObject.Parse(result);
+                switch ((string)jobj["result"])
+                {
+                    case "success":
+                        MessageBox.Show("Success!");
+                        break;
+                    case "failed":
+                        MessageBox.Show("Failed!");
+                        break;
+                    default:
+                        MessageBox.Show("Unknown error!");
+                        break;
+                }
+            }
+        }
+
+        private static void GetData()
+        {
+            var values = new NameValueCollection();
+            values["type"] = "t_get_data";
+            string result = Handler.DoPost(values);
+            MessageBox.Show(result);
+            if (result != "")
+            {
+                var jobj = JObject.Parse(result);
+                switch ((string)jobj["result"])
+                {
+                    case "success":
+                        MessageBox.Show("Success!" + Environment.NewLine + "Data: " + (string)jobj["data"]);
+                        break;
+                    case "failed":
+                        MessageBox.Show("Failed!");
+                        break;
+                    default:
+                        MessageBox.Show("Unknown error!");
+                        break;
+                }
+            }
+        }
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            InsertData("SETO_KOUJI");
+        }
+
+        private void BtnGet_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+    }
+}
